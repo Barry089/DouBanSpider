45633036ceea38d589ae27e011aef053eeec45bf
Barry089 <lizhe_email@163.com>
2019-12-18 22:02:26 +0800

    add book lists_excel func.

--- doubanSpider.py
+++ doubanSpider.py
@@ -6,7 +6,7 @@ from urllib.parse import quote_plus
 # import requests
 import numpy as np
 from bs4 import BeautifulSoup
-# from openpyxl import Workbook
+from openpyxl import Workbook
 
 
 reload(sys)
@@ -102,6 +102,24 @@ def do_spider(book_tag_lists):
         book_lists.append(book_list)
 
     return book_lists
+
+
+def print_book_lists_excel(book_lists, book_tag_lists):
+    wb = Workbook(optimized_write=True)
+    ws = []
+    for i in range(len(book_tag_lists)):
+        ws.append(wb.create_sheet(title=book_tag_lists[i].decode()))
+    for i in range(len(book_tag_lists)):
+        ws[i].append(['序号', '书名', '评分', '评价人数', '作者', '出版社'])
+        count=1
+        for bl in book_lists[i]:
+            ws[i].append([count, bl[0], float(bl[1]), int(bl[2]), bl[3], bl[4]])
+            count += 1
+    save_path = 'book_list'
+    for i in range(len(book_tag_lists)):
+        save_path += ('_' + book_tag_lists[i].decode())
+    save_path += '.xlsx'
+    wb.save(save_path)
 
 
 if __name__ == '__main__':
